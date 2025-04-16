a6bc399271248c40d96f2742c677bdee51d933ec
mogaigor <moga.igor@iisgalvanimi.edu.it>
2025-04-16 12:43:48 +0000

    s1

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template, request, redirect, url_for #importiamo la classe flask
+app = Flask(__name__) #inizializza app flask
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True) #aggiornamenti in tempo reale
